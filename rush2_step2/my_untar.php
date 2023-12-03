39f3114cce71b0d14373c805b86b25de6445d01c
jud3v2 <judikaelbellance@icloud.com>
2023-12-03 14:04:43 +0100

    ajout de la possibilité de décompressé des fichier encodé en base64 t'elle que les images les pdf les vidéo

--- rush2_step2/my_untar.php
+++ rush2_step2/my_untar.php
@@ -1,36 +1,52 @@
 <?php
 
+// Affiche un message dans la console
 function logMessage($message) {
     echo $message . PHP_EOL;
 }
 
+// Affiche un menu de choix pour l'utilisateur et récupère la sélection
 function askUserChoice() {
     fwrite(STDOUT, "Choisissez une option :\n1. Écraser\n2. Ne pas écraser\n3. Écraser pour tous\n4. Ne pas écraser pour tous\n5. Arrêter et quitter\nVotre choix : ");
     return trim(fgets(STDIN));
 }
 
+// Vérifie si une chaîne est encodée en base64
+function isBase64Encoded($data) {
+    if (preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data)) {
+        return base64_decode($data, true) !== false;
+    }
+    return false;
+}
+
+// Extrait les fichiers d'une archive .mytar
 function extractMyTar($filePath) {
     $overwriteAll = false;
     $skipAll = false;
 
+    // Vérifier si le fichier .mytar existe
     if (!file_exists($filePath)) {
         return logMessage("Erreur : Le fichier $filePath n'existe pas.");
     }
 
+    // Lire le contenu du fichier et le décoder du JSON
     $fileContent = file_get_contents($filePath);
     $filesData = json_decode($fileContent, true);
 
+    // Vérifier si le contenu JSON est valide
     if ($filesData === null) {
         return logMessage("Erreur : Le contenu du fichier $filePath n'est pas un JSON valide.");
     }
 
+    // Traitement de chaque fichier dans les données JSON
     foreach ($filesData as $fileData) {
-        $fileName = $fileData['name'];
         $fileRelativePath = $fileData['path'];
         $fileContent = $fileData['content'];
 
+        // Construction du chemin complet du fichier
         $fullPath = dirname($filePath) . '/' . $fileRelativePath;
 
+        // Gestion des conflits de fichiers
         if (file_exists($fullPath)) {
             if (!$overwriteAll && !$skipAll) {
                 logMessage("Conflit avec le fichier/dossier : $fullPath");
@@ -57,20 +73,32 @@ function extractMyTar($filePath) {
             }
         }
 
+        // Création des répertoires si nécessaire
         $directory = dirname($fullPath);
         if (!is_dir($directory)) {
             mkdir($directory, 0777, true);
-            logMessage("Répertoire créé : ".basename($directory)."/");
+            logMessage("Répertoire créé : $directory");
+        }
+
+        // Décodage du contenu si nécessaire et écriture dans le fichier
+        if (isBase64Encoded($fileContent)) {
+            $fileContent = base64_decode($fileContent);
         }
 
         file_put_contents($fullPath, $fileContent);
-        logMessage("Fichier extrait : ".basename($fullPath));
+        logMessage("Fichier extrait : $fullPath");
     }
 
     return logMessage("Extraction terminée avec succès.");
 }
 
-fwrite(STDOUT, "Entrez le chemin du fichier .mytar à décompresser : ");
-$filePath = trim(fgets(STDIN));
+// Récupération du chemin du fichier depuis la ligne de commande ou entrée utilisateur
+$filePath = $argc > 1 ? $argv[1] : null;
 
+if (!$filePath) {
+    fwrite(STDOUT, "Entrez le chemin du fichier .mytar à décompresser : ");
+    $filePath = trim(fgets(STDIN));
+}
+
+// Appel de la fonction d'extraction avec le chemin du fichier
 extractMyTar($filePath);
