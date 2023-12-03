9ad5d8d437bb2dd52b992f632485692bf00ccb22
jud3v2 <judikaelbellance@icloud.com>
2023-12-03 16:25:43 +0100

    ajout des modification pour le téléchargement et l'envoie de fichier de nextjs vers laravel

--- rush2_step3/frontend/app/_components/dragAndDrop.tsx
+++ rush2_step3/frontend/app/_components/dragAndDrop.tsx
@@ -1,151 +1,193 @@
 "use client";
 
-import { useRef, useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import {useRef, useEffect, useState} from "react";
+import {toast} from "react-toastify";
+import {useMutation} from "@tanstack/react-query";
+import axios from 'axios';
 
-export default function DragAndDrop() {
-  const [dragActive, setDragActive] = useState<boolean>(false);
-  const inputRef = useRef<any>(null);
-  const [files, setFiles] = useState<any>([]);
+export default function DragAndDrop({onClickSend, onClickDownloadTarball, setOnClickSend}: { onClickSend: any, onClickDownloadTarball: any, setOnClickSend: Function}) {
+    const [dragActive, setDragActive] = useState<boolean>(false);
+    const inputRef = useRef<any>(null);
+    const [files, setFiles] = useState<any>([]);
 
-  useEffect(() => {
-    if (inputRef.current !== null) {
-      // 2. set attribute as JS does
-      inputRef.current.setAttribute("directory", "");
-      inputRef.current.setAttribute("webkitdirectory", "");
+    useEffect(() => {
+        if (inputRef.current !== null) {
+            // 2. set attribute as JS does
+            inputRef.current.setAttribute("directory", "");
+            inputRef.current.setAttribute("webkitdirectory", "");
+        }
+        // 3. monitor change of your ref with useEffect
+    }, [inputRef]);
+
+    const insert_file_to_server = async (files: any) => {
+        if (files.length === 0) {
+            toast("Aucun fichier n'a été ajouté, annulation de l'envoie du formulaire.", {
+                type: 'error'
+            })
+            return;
+        } else {
+            const fd = new FormData();
+
+            files.forEach((file: any, index: any) => {
+                fd.append(`files[${index}]`, file);
+            });
+
+            return await axios.post('http://localhost:8000/api/tar/make', fd, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+                .then(({data}) => {
+                    toast("Votre tarball à bien été générer", {
+                        type: 'success'
+                    })
+                    return data
+                })
+                .catch(e => {
+                    console.log(e)
+                    toast("erreur lors de la génération de votre tarball", {
+                        type: 'error'
+                    })
+                })
+        }
     }
-    // 3. monitor change of your ref with useEffect
-  }, [inputRef]);
 
-  function handleChange(e: any) {
-    e.preventDefault();
-    console.log("File has been added");
-    if (e.target.files && e.target.files[0]) {
-      for (let i = 0; i < e.target.files["length"]; i++) {
-        setFiles((prevState: any) => [...prevState, e.target.files[i]]);
-      }
+    const mutation = useMutation({
+        mutationFn: insert_file_to_server,
+        onError: () => {
+            toast("Une erreur est survenue lors de l'envoie de vos fichier")
+        }
+    })
+
+    useEffect(() => {
+        if (onClickSend !== null) {
+            mutation.mutate(files)
+            setOnClickSend(null); // prevent second send without anything
+        }
+    }, [onClickSend])
+
+    function handleChange(e: any) {
+        e.preventDefault();
+        console.log("File has been added");
+        if (e.target.files && e.target.files[0]) {
+            for (let i = 0; i < e.target.files["length"]; i++) {
+                setFiles((prevState: any) => [...prevState, e.target.files[i]]);
+            }
+        }
     }
-  }
 
-  function handleSubmitFile(e: any) {
-    if (files.length === 0) {
-      // no file has been submitted
-    } else {
-      // write submit logic here
+
+    function handleDrop(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        setDragActive(false);
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            for (let i = 0; i < e.dataTransfer.files["length"]; i++) {
+                setFiles((prevState: any) => [...prevState, e.dataTransfer.files[i]]);
+            }
+        }
     }
-  }
 
-  function handleDrop(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      for (let i = 0; i < e.dataTransfer.files["length"]; i++) {
-        setFiles((prevState: any) => [...prevState, e.dataTransfer.files[i]]);
-      }
+    function handleDragLeave(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        setDragActive(false);
     }
-  }
 
-  function handleDragLeave(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-  }
-
-  function handleDragOver(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(true);
-  }
-
-  function handleDragEnter(e: any) {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(true);
-  }
-
-  function removeFile(fileName: any, idx: any) {
-    const newArr = [...files];
-    newArr.splice(idx, 1);
-    setFiles([]);
-    setFiles(newArr);
-    toast(`Le fichier ${fileName} à bien été retirer de la liste`)
-  }
-
-  function openFileExplorer() {
-    inputRef.current.value = "";
-    inputRef.current.click();
-  }
-
-  function removeAllFiles() {
-    if (window.confirm("Êtes vous sûr de vouloir supprimer de la liste tous les fichiers importé ?")) {
-      setFiles([]);
-      toast("Les fichiers précédement envoyé ont bien été vider de la liste")
+    function handleDragOver(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        setDragActive(true);
     }
-  }
 
-  return (
-    <div className="flex items-center justify-center w-full my-2">
-      <form
-        className={`${dragActive ? "bg-blue-400" : "bg-blue-500"
-          }  p-4 w-100 rounded-lg border-solid border-2 border-black text-center borderff flex flex-col items-center w-100 justify-center`}
-        onDragEnter={handleDragEnter}
-        onSubmit={(e) => e.preventDefault()}
-        onDrop={handleDrop}
-        onDragLeave={handleDragLeave}
-        onDragOver={handleDragOver}
-      >
+    function handleDragEnter(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        setDragActive(true);
+    }
 
-        <input
-          placeholder="fileInput"
-          className="hidden"
-          ref={inputRef}
-          type="file"
-          multiple={true}
-          onChange={handleChange}
-        />
+    function removeFile(fileName: any, idx: any) {
+        const newArr = [...files];
+        newArr.splice(idx, 1);
+        setFiles([]);
+        setFiles(newArr);
+        toast(`Le fichier ${fileName} à bien été retirer de la liste`)
+    }
 
-        <p className="font-bold">
-          Drag & Drop vos dossiers ou{" "}
-          <span
-            className="font-bold text-red-400 cursor-pointer"
-            onClick={openFileExplorer}
-          >
+    function openFileExplorer() {
+        inputRef.current.value = "";
+        inputRef.current.click();
+    }
+
+    function removeAllFiles() {
+        if (window.confirm("Êtes vous sûr de vouloir supprimer de la liste tous les fichiers importé ?")) {
+            setFiles([]);
+            toast("Les fichiers précédement envoyé ont bien été vider de la liste")
+        }
+    }
+
+    return (
+        <div className="flex items-center justify-center w-full my-2">
+            <form
+                className={`${dragActive ? "bg-blue-400" : "bg-blue-500"
+                }  p-4 w-100 rounded-lg border-solid border-2 border-black text-center borderff flex flex-col items-center w-100 justify-center`}
+                onDragEnter={handleDragEnter}
+                onSubmit={(e) => e.preventDefault()}
+                onDrop={handleDrop}
+                onDragLeave={handleDragLeave}
+                onDragOver={handleDragOver}
+            >
+
+                <input
+                    placeholder="fileInput"
+                    className="hidden"
+                    ref={inputRef}
+                    type="file"
+                    multiple={true}
+                    onChange={handleChange}
+                />
+
+                <p className="font-bold">
+                    Drag & Drop vos dossiers ou{" "}
+                    <span
+                        className="font-bold text-red-400 cursor-pointer"
+                        onClick={openFileExplorer}
+                    >
             <u>Séléctionner vos dossiers</u>
           </span>{" "}
-          pour les transformer en tarball
-        </p>
+                    pour les transformer en tarball
+                </p>
 
-        <div className="flex flex-col items-center p-3 overflow-auto h-48 min-h-full">
-          {files.map((file: any, idx: any) => (
-            <div key={idx} className="flex flex-row flex-gap-1 justify-between space-x-5 ">
-              <span className="flex">{file.name}</span>
-              <span
-                style={{
-                  padding: '.5px',
-                  cursor: 'pointer'
-                }}
-                className="bg-red-500 flex hover:bg-red-700 text-white font-bold rounded m-1"
-                onClick={() => removeFile(file.name, idx)}
-              >
+                <div className="flex flex-col items-center p-3 overflow-auto h-48 min-h-full">
+                    {files.map((file: any, idx: any) => (
+                        <div key={idx} className="flex flex-row flex-gap-1 justify-between space-x-5 ">
+                            <span className="flex">{file.name}</span>
+                            <span
+                                style={{
+                                    padding: '.5px',
+                                    cursor: 'pointer'
+                                }}
+                                className="bg-red-500 flex hover:bg-red-700 text-white font-bold rounded m-1"
+                                onClick={() => removeFile(file.name, idx)}
+                            >
                 Supprimer
               </span>
-            </div>
-          ))}
-        </div>
+                        </div>
+                    ))}
+                </div>
 
-        {files.length > 0 ? <span
-          style={{
-            padding: '2px',
-            cursor: 'pointer'
-          }}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold rounded m-1"
-          onClick={() => removeAllFiles()}
-        >
+                {files.length > 0 ? <span
+                    style={{
+                        padding: '2px',
+                        cursor: 'pointer'
+                    }}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold rounded m-1"
+                    onClick={() => removeAllFiles()}
+                >
           Supprimer tous les fichiers
         </span> : ''}
-      </form>
+            </form>
 
-    </div>
-  );
+        </div>
+    );
 }
--- rush2_step3/frontend/app/page.tsx
+++ rush2_step3/frontend/app/page.tsx
@@ -1,145 +1,178 @@
 "use client"
 
 import styles from './page.module.css'
-import { TextField } from '@mui/material'
+import {TextField} from '@mui/material'
 import DragAndDrop from './_components/dragAndDrop'
-import { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
+import {useState} from 'react'
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {useQuery} from "@tanstack/react-query";
 import axios from 'axios';
 
 export default function Home() {
-  const [nameOfFile, setNameOfFile] = useState<string>('');
+    const [nameOfFile, setNameOfFile] = useState<string>('');
+    const [onClickSend, setOnClickSend] = useState<any>(null);
+    const [onClickDownloadTarball, setOnClickDownloadTarball] = useState<any>(null);
+    const try_to_connect_to_api = async () => {
+        return await axios.get('http://localhost:8000/api/ping')
+            .then(({data}) => {
+                toast("ping serveur réussi", {
+                    type: 'success'
+                })
+                return data
+            })
+            .catch((e) => {
+                toast("ping serveur impossible", {
+                    type: "error"
+                })
+                return e;
+            })
+    }
 
-  const try_to_connect_to_api =  async () => {
-    return await axios.get('http://localhost:8000/api/ping')
-        .then(({data}) => {
-          toast("connexion au serveur réussi")
-          return data
-        })
-        .catch((e) => {
-          toast("Une erreur est survenue lors de la connexion au serveur", {
-            type: "error"
-          })
-          return e;
-        })
-  }
+    const downloadFile = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/tar/download', {
+                responseType: 'blob',
+            });
 
-  useQuery({queryKey: ["first_connexion"], queryFn: try_to_connect_to_api})
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'output.mytar');
+            document.body.appendChild(link);
+            link.click();
+            toast("Téléchargement de votre archive réussi.");
+        } catch (error) {
+            toast("Merci de bien vouloir générer une tarball avant téléchargement.", {
+                type: "error"
+            })
+        }
+    };
 
-  return (
-    <main className={styles.main}>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <div className={styles.description}>
-        <p className='font-bold text-md'>
-          Rush 2 Web@cademie By Epitech
-        </p>
-        <div>
-          <a
-            href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main"
-            target="_blank"
-            rel="noopener noreferrer"
-            className='font-bold text-md'
-          >
-            By{' '}
-            Med, Enzo And Judikaël
-          </a>
-        </div>
-      </div>
+    useQuery({queryKey: ["first_connexion"], queryFn: try_to_connect_to_api})
 
-      <div className='flex'>
-        <div className='m-2 flex-initial w-100'>
-          <div>
-            <h3 className='font-bold text-2xl my-2'>Utiliser le drag and drop de fichier afin d&apos;envoyer vos fichers </h3>
-          </div>
-          <div>
-            <TextField 
-            fullWidth 
-            id="filename" 
-            className='my-2'
-            value={nameOfFile}
-            variant='outlined' 
-            onChange={e => setNameOfFile(e.target.value)}
-            label="Choisissez le nom de votre tarball"  />
-          </div>
-          <div className='p-1 flex justify-around'>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1'>Générer la tarball</button>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1'>Télécharger la tarball</button>
-          </div>
-          <div>
-            <DragAndDrop />
-          </div>
-        </div>
-      </div>
+    return (
+        <main className={styles.main}>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <div className={styles.description}>
+                <p className='font-bold text-md'>
+                    Rush 2 Web@cademie By Epitech
+                </p>
+                <div>
+                    <a
+                        href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='font-bold text-md'
+                    >
+                        By{' '}
+                        Med, Enzo And Judikaël
+                    </a>
+                </div>
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step1"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className='font-bold text-xl'>
-            Étape 1<span>-&gt;</span>
-          </h2>
-          <p>Création d&apos;une tarball.</p>
-        </a>
+            <div className='flex'>
+                <div className='m-2 flex-initial w-100'>
+                    <div>
+                        <h3 className='font-bold text-2xl my-2'>Utiliser le drag and drop de fichier afin d&apos;envoyer
+                            vos fichers </h3>
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            id="filename"
+                            className='my-2'
+                            value={nameOfFile}
+                            variant='outlined'
+                            onChange={e => setNameOfFile(e.target.value)}
+                            label="Choisissez le nom de votre tarball"/>
+                    </div>
+                    <div className='p-1 flex justify-around'>
+                        <button
+                            onClick={setOnClickSend}
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1'>Générer
+                            la tarball
+                        </button>
+                        <button
+                            onClick={downloadFile}
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1'>Télécharger
+                            la tarball
+                        </button>
+                    </div>
+                    <div>
+                        <DragAndDrop onClickSend={onClickSend} onClickDownloadTarball={onClickDownloadTarball} setOnClickSend={setOnClickSend}/>
+                    </div>
+                </div>
+            </div>
 
-        <a
-          href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step2"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className='font-bold text-xl'>
-            Étape 2 <span>-&gt;</span>
-          </h2>
-          <p>Création de la décompression d&apos;une tarball.</p>
-        </a>
+            <div className={styles.grid}>
+                <a
+                    href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step1"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className='font-bold text-xl'>
+                        Étape 1<span>-&gt;</span>
+                    </h2>
+                    <p>Création d&apos;une tarball.</p>
+                </a>
 
-        <a
-          href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step3"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className='font-bold text-xl'>
-            Étape 3 <span>-&gt;</span>
-          </h2>
-          <p>Ceci est le site que vous visitez actuellement qui consiste à être un site web qui créé et décompresse des tarballs.</p>
-        </a>
+                <a
+                    href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step2"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className='font-bold text-xl'>
+                        Étape 2 <span>-&gt;</span>
+                    </h2>
+                    <p>Création de la décompression d&apos;une tarball.</p>
+                </a>
 
-        <a
-          href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_bonus"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className='font-bold text-xl'>
-            Bonus <span>-&gt;</span>
-          </h2>
-          <p>
-            Amélioration de l&apos;algorithme de compression.
-          </p>
-        </a>
-      </div>
-      <footer>
-          <p className='font-bold my-2'>
-            {new Date().getFullYear()} © Made By Med, Enzo And Judikaël
-          </p>
-      </footer>
-    </main>
-  )
+                <a
+                    href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_step3"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className='font-bold text-xl'>
+                        Étape 3 <span>-&gt;</span>
+                    </h2>
+                    <p>Ceci est le site que vous visitez actuellement qui consiste à être un site web qui créé et
+                        décompresse des tarballs.</p>
+                </a>
+
+                <a
+                    href="https://github.com/EpitechWebAcademiePromo2025/W-WEB-024-LIL-1-1-rush2-judikael2.bellance/tree/main/rush2_bonus"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className='font-bold text-xl'>
+                        Bonus <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Amélioration de l&apos;algorithme de compression.
+                    </p>
+                </a>
+            </div>
+            <footer>
+                <p className='font-bold my-2'>
+                    {new Date().getFullYear()} © Made By Med, Enzo And Judikaël
+                </p>
+            </footer>
+        </main>
+    )
 }
